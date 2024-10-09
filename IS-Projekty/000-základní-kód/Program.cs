425ddaef9287524413a7123a4da0df3b7ce2a8d6
Leoš Beran <leos.beran1@tul.cz>
2024-10-09 09:45:18 +0200

    #1 základní kód

---
+++ IS-Projekty/000-základní-kód/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        string again = "a";
+
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("***************");
+            Console.WriteLine("*** VÝPIS ŘADY ČÍSEL ***");
+            Console.WriteLine("***************");
+            Console.WriteLine("**** Leoš Beran ****");
+            Console.WriteLine("***************");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+
+// toto je jednořádkový komentář
+
+/* toto je
+víceřádkový
+komentář */
