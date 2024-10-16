8e80ddc3bca385c4292f6d15ba173264ad5628e5
Leoš Beran <leos.beran1@tul.cz>
2024-10-16 09:59:28 +0200

    #2 Další úpravy

--- IS-Projekty/Program.cs
+++ IS-Projekty/Program.cs
@@ -26,8 +26,29 @@ class Program {
             }
 
             
+            /* možné řešení
+            int suma = 0;
+            multi =
+            int numberBackup = number;
+            int digit;
 
+            while (number >= 10) {
+                digit = number % 10;
+                number = (number-digit)/10;
+                Console.WriteLine("Digit = {0}" , digit);
+                suma = suma + digit;
+                multi = multi * digit;
+            }
+            Console.WriteLine("Digit = {0}" , digit);
+           
+            // musíme přičíst ještě poslední cifru
+            suma = suma + number;
+            multi = multi * number;
 
+            Console.WriteLine();
+            Console.WriteLine("\n\n\Součin cifer čísla {0} je {1}", numberBackup, multi) ;
+
+            */
 
             int suma = 0;
             int numberBackup = number;
