13cfe8b2cf1b953d83cf1fdbdc3fc49968a315cc
Leoš Beran <leos.beran1@tul.cz>
2024-10-16 19:36:47 +0200

    #2 Domácí úkol - finální verze

--- IS-Projekty/003b - Pravoúhlý trojúhelník/obj/Debug/net8.0/003b - Pravoúhlý trojúhelník.AssemblyInfo.cs
+++ IS-Projekty/003b - Pravoúhlý trojúhelník/obj/Debug/net8.0/003b - Pravoúhlý trojúhelník.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("003b - Pravoúhlý trojúhelník")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b09997d900b9f6ae68de04012bbb97ca800d8d12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4c5d8130fe8f597a149328f2dc9f04f3a37b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("003b - Pravoúhlý trojúhelník")]
 [assembly: System.Reflection.AssemblyTitleAttribute("003b - Pravoúhlý trojúhelník")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
