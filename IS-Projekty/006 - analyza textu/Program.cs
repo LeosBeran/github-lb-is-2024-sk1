9d64d210c0d340d7850d724aa4b59d2ad680b94d
Leoš Beran <leos.beran1@tul.cz>
2024-11-06 10:14:33 +0100

    #6 analyza textu

---
+++ IS-Projekty/006 - analyza textu/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        string again = "a";
+
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("***************");
+            Console.WriteLine("*** ANALYZA TEXTU ***");
+            Console.WriteLine("***************");
+            Console.WriteLine("**** Leoš Beran ****");
+            Console.WriteLine("***************");
+            Console.WriteLine();
+
+            Console.Write("\nZadejte váš text: ");
+            string myText = Console.ReadLine();
+            
+            string samohlasky = "aáeéěiíoóuůúyý";
+            string souhlasky = "bcčdďfghjklmnňpqrřsštťvwxzž";
+            string cislice = "0123456789";
+
+            int pocetSamohlasek = 0;
+            int pocetSouhlasek = 0;
+            int pocetOstatnich = 0;
+            int pocetCislic = 0;
+
+            foreach(char znak in myText) {
+                if(souhlasky.Contains(znak)){
+                    pocetSouhlasek++;
+                }
+                else if(samohlasky.Contains(znak)){
+                    pocetSamohlasek++;
+                }
+                else if(cislice.Contains(znak)){
+                    pocetCislic++;
+                }
+                else 
+                    pocetOstatnich++;
+                
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Počet samohlásek: {0}", pocetSamohlasek) ;
+            Console.WriteLine("Počet souhlásek: {0}", pocetSouhlasek); 
+            Console.WriteLine("Počet číslic: {0}", pocetCislic); 
+            Console.WriteLine("Počet ostatních znaků: {0}", pocetOstatnich) ;
+
+
+
+        
+
+            Console.WriteLine();
+            Console.WriteLine(myText);
+            Console.WriteLine(myText[0]);
+            Console.WriteLine(myText.Length);
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+
+// toto je jednořádkový komentář
+
+/* toto je
+víceřádkový
+komentář */
